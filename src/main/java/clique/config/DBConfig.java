53a40a0afef1d2de96de6edb71c433d71d117b59
Gal Schlezinger <galstar@spitfire.co.il>
2016-02-05 18:24:36 +0200

    added stuff

--- src/main/java/clique/config/DBConfig.java
+++ src/main/java/clique/config/DBConfig.java
@@ -6,17 +6,17 @@ import com.rethinkdb.RethinkDB;
 import com.rethinkdb.net.Connection;
 
 public class DBConfig {
-	private final static String hostname = "192.168.99.100";
-	private final static int port = 32769;
+	private final static String hostname = "127.0.0.1";
+	private final static int port = 28015;
 	private final static String dbName = "test";
 	
 	public static final RethinkDB r = RethinkDB.r;
-	
+
 	private static String hostname()
 	{
 		return hostname;
 	}
-	
+
 	private static int port()
 	{
 		return port;
@@ -30,6 +30,7 @@ public class DBConfig {
 	public static Connection get()
 	{
 		try {
+			System.out.println(hostname() + ":" + port() + "/" + dbName());
 			return r.connection().hostname(hostname()).port(port()).db(dbName()).connect();
 		} catch (TimeoutException e) {
 			throw new RuntimeException();
