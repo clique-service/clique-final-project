d2023a471ae414e049d0a5d608507b80d9df5bfb
tomBold <tomboldan@gmail.com>
2016-02-06 18:22:26 +0200

    init project

--- src/main/java/clique/helpers/JsonToPureJava.java
+++ src/main/java/clique/helpers/JsonToPureJava.java
@@ -4,26 +4,32 @@ import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
- * Created by schniz on 05/02/2016.
- * Converts Vert.x 3 JsonArray to pure java
- * for using it with RethinkDB
+ * Created by schniz on 05/02/2016. Converts Vert.x 3 JsonArray to pure java for
+ * using it with RethinkDB
  */
 public class JsonToPureJava {
 	public static Object toJava(Object json) {
 		if (json instanceof JsonObject) {
-			Map<String, Object> map = ((JsonObject) json).getMap();
-			Map<String, Object> newMap = new HashMap<>();
+			return toJava(((JsonObject) json).getMap());
+		} else if (json instanceof JsonArray) {
+			return toJava(((JsonArray) json).getList());
+		} else if (json instanceof String) {
+			return ((String) json).toLowerCase();
+		} else if (json instanceof List<?>) {
+			return ((List<?>) json).stream().map(JsonToPureJava::toJava).toArray();
+		} else if (json instanceof Map<?, ?>) {
+			Map<?, ?> map = (Map<?, ?>) json;
+			Map<Object, Object> newMap = new HashMap<>();
 
-			for (String key : map.keySet()) {
+			for (Object key : map.keySet()) {
 				newMap.put(key, toJava(map.get(key)));
 			}
 
 			return newMap;
-		} else if (json instanceof JsonArray) {
-			return ((JsonArray) json).stream().map(JsonToPureJava::toJava).toArray();
 		}
 
 		return json;
