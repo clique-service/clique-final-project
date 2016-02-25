a97d3377745f7f6a65811c4ccad2e7bea175de75
tomBold <tomboldan@gmail.com>
2016-02-25 22:31:37 +0200

    blocking

--- src/main/java/clique/verticles/SharedTableCreateHandler.java
+++ src/main/java/clique/verticles/SharedTableCreateHandler.java
@@ -19,34 +19,40 @@ public class SharedTableCreateHandler extends AbstractVerticle {
 		vertx.eventBus().<String> consumer(getHandlerName(), message -> {
 			String userId = message.body();
 
-			String tableName = userId + "Shared";
-			DBConfig.execute(r.tableCreate(tableName));
+			vertx.executeBlocking(future -> {
+				String tableName = userId + "Shared";
+				DBConfig.execute(r.tableCreate(tableName));
 
-			DBConfig.execute(r.table(tableName).indexCreate("rating", user -> user.g("events").mul(5)
-					.add(user.g("likes").mul(3)).add(user.g("places").mul(2)).add(user.g("categories").mul(2))));
+				DBConfig.execute(r.table(tableName).indexCreate("rating", user -> user.g("events").mul(5)
+						.add(user.g("likes").mul(3)).add(user.g("places").mul(2)).add(user.g("categories").mul(2))));
 
-			// TODO: Start gets changes
+				// TODO: Start gets changes
 
-			ReqlExpr user = r.table("Users").get(userId).coerceTo("object");
+				ReqlExpr user = r.table("Users").get(userId).coerceTo("object");
 
-			ReqlExpr dataToShare = r.table("Users").map(otherUser -> {
-				return r.hashMap().with("id", otherUser.g("id")).with("name", otherUser.g("name"))
-						.with("events", getIntersection("events", user, otherUser))
-						.with("likes", getIntersection("likes", user, otherUser))
-						.with("places", getIntersection("places", user, otherUser))
-						.with("categories", getIntersection("categories", user, otherUser));
-			}).filter(otherUser -> otherUser.g("id").eq(userId).not().and(otherUser.g("events").gt(0)
-					.or(otherUser.g("likes").gt(0), otherUser.g("places").gt(0), otherUser.g("categories").gt(0))));
+				ReqlExpr dataToShare = r.table("Users").map(otherUser -> {
+					return r.hashMap().with("id", otherUser.g("id")).with("name", otherUser.g("name"))
+							.with("events", getIntersection("events", user, otherUser))
+							.with("likes", getIntersection("likes", user, otherUser))
+							.with("places", getIntersection("places", user, otherUser))
+							.with("categories", getIntersection("categories", user, otherUser));
+				}).filter(otherUser -> otherUser.g("id").eq(userId).not().and(otherUser.g("events").gt(0)
+						.or(otherUser.g("likes").gt(0), otherUser.g("places").gt(0), otherUser.g("categories").gt(0))));
 
-			DBConfig.execute(r.table(tableName).insert(dataToShare));
+				DBConfig.execute(r.table(tableName).insert(dataToShare));
 
-			ReqlExpr sortedResults = r.table(tableName).orderBy().optArg("index", r.desc("rating")).limit(5)
-					.coerceTo("array");
-			DBConfig.execute(r.table("CliqueResults")
-					.insert(r.hashMap().with("userId", userId).with("date", r.now()).with("results", sortedResults)));
+				ReqlExpr sortedResults = r.table(tableName).orderBy().optArg("index", r.desc("rating")).limit(5)
+						.coerceTo("array");
+				DBConfig.execute(r.table("CliqueResults").insert(
+						r.hashMap().with("userId", userId).with("date", r.now()).with("results", sortedResults)));
 
-			DBConfig.execute(r.tableDrop(tableName));
+				DBConfig.execute(r.tableDrop(tableName));
+				future.complete();
+			} , res -> {
+				System.out.println("Finish shared result");
+			});
 		});
+
 	}
 
 	private ReqlExpr getIntersection(String field, ReqlExpr user, ReqlExpr otherUser) {
