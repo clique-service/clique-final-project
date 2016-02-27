36e3bb2411c8e99653b6566ac447da17856b12a2
tomBold <tomboldan@gmail.com>
2016-02-27 11:57:49 +0200

    top5

--- src/main/java/clique/verticles/TopMatchesChanges.java
+++ src/main/java/clique/verticles/TopMatchesChanges.java
@@ -2,16 +2,38 @@ package clique.verticles;
 
 import static com.rethinkdb.RethinkDB.r;
 
+import com.rethinkdb.gen.ast.Limit;
+import com.rethinkdb.gen.ast.ReqlExpr;
 import com.rethinkdb.net.Cursor;
 
+import clique.config.DBConfig;
 import rx.Observable;
 import rx.Observer;
 
 public class TopMatchesChanges {
-	public Observable<String> getTop5(String id) {
-		
-		
-		
-		return null;
+	private String userId;
+	private Observable<Object> observable;
+	private Cursor<Object> cursor;
+
+	public TopMatchesChanges(String userId) {
+		this.userId = userId;
+	}
+
+	public Observable<Object> getObservable() {
+		return observable;
+	}
+
+	public void close() {
+		this.cursor.close();
+	}
+
+	public void run() {
+		ReqlExpr five = r.table(userId + "Shared").orderBy().optArg("index", r.desc("rating")).limit(5);
+
+		this.cursor = DBConfig.execute(five.changes().optArg("squash", true).map(x -> {
+			return five.coerceTo("array");
+		}));
+
+		this.observable = Observable.from(this.cursor);
 	}
 }
