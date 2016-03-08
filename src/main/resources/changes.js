9e0cb0c1b064476d79d5c066c78bc86850fe4171
tomBold <tomboldan@gmail.com>
2016-03-08 20:21:33 +0200

    f

--- src/main/resources/changes.js
+++ src/main/resources/changes.js
@@ -16,5 +16,30 @@ setInterval(function(){
   getChanges().then(function(data)
   {
     console.log(data);
+    updateUsersDiv(data);
   })
 }, 1000);
+
+function buildUser(user)
+{
+  var pic = $("<img>").src("http://graph.facebook.com/" + user.id + "/picture?width=150&type=square");
+  var name = $("<span>").text(user.name);
+  var likes = $("<span>").text("likes: " + user.likes);
+  var categories = $("<span>").text("categories: " + user.categories);
+  var events = $("<span>").text("events: " + user.events);
+  var places = $("<span>").text("places: " + user.places);
+
+  var user = $("<div>").append(pic, name, likes, categories, events, places);
+
+  return user;
+}
+
+function buildUsers(users)
+{
+  return users.map(buildUser);
+}
+
+function updateUsersDiv(users)
+{
+  $("#users").empty().append(buildUsers(users));
+}
