c2952b020d434aeacb02b9bf3188920a0ca64d83
Gal Schlezinger <galstar@spitfire.co.il>
2016-03-08 20:40:47 +0200

    Changed

--- src/main/resources/changes.js
+++ src/main/resources/changes.js
@@ -22,14 +22,13 @@ setInterval(function(){
 
 function buildUser(user)
 {
-  var pic = $("<img>").src("http://graph.facebook.com/" + user.id + "/picture?width=150&type=square");
-  var name = $("<span>").text(user.name);
-  var likes = $("<span>").text("likes: " + user.likes);
-  var categories = $("<span>").text("categories: " + user.categories);
-  var events = $("<span>").text("events: " + user.events);
-  var places = $("<span>").text("places: " + user.places);
-
-  var user = $("<div>").append(pic, name, likes, categories, events, places);
+  var pic = $("<img>").addClass("profile-picture").attr("src", "http://graph.facebook.com/" + user.id + "/picture?width=150&type=square");
+  var name = $("<span>").addClass("name").text(user.name);
+  var likes = $("<span>").addClass("details").addClass("likes").text("likes: " + user.likes);
+  var categories = $("<span>").addClass("details").addClass("categories").text("categories: " + user.categories);
+  var events = $("<span>").addClass("details").addClass("events").text("events: " + user.events);
+  var places = $("<span>").addClass("details").addClass("places").text("places: " + user.places);
+  var user = $("<div>").addClass("user").append(pic, name, likes, categories, events, places);
 
   return user;
 }
