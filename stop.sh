b4e467f6ad06c5ee7621d3c82b9c3f801047cf8a
tomBold <tomboldan@gmail.com>
2016-03-17 20:24:05 +0200

    fix stop script

--- stop.sh
+++ stop.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-java -jar ./EventAttendeesHandler/target/EventAttendeesHandler-1.0-SNAPSHOT-fat.jar stop -cluster clique.verticles.EventAttendeesHandler
-java -jar ./EventInterestedsHandler/target/EventInterestedsHandler-1.0-SNAPSHOT-fat.jar stop -cluster clique.verticles.EventInterestedsHandler
-java -jar ./EventMaybesHandler/target/EventMaybesHandler-1.0-SNAPSHOT-fat.jar stop -cluster clique.verticles.EventMaybesHandler
-java -jar ./LikePostsHandler/target/LikePostsHandler-1.0-SNAPSHOT-fat.jar stop -cluster clique.verticles.LikePostsHandler
-java -jar ./PostLikesHandler/target/PostLikesHandler-1.0-SNAPSHOT-fat.jar stop -cluster clique.verticles.PostLikesHandler
-java -jar ./SharedTableDataInsertionHandler/target/SharedTableDataInsertionHandler-1.0-SNAPSHOT-fat.jar stop -cluster clique.verticles.SharedTableDataInsertionHandler
-java -jar ./UserEventsHandler/target/UserEventsHandler-1.0-SNAPSHOT-fat.jar stop -cluster clique.verticles.UserEventsHandler
-java -jar ./UserInitHandler/target/UserInitHandler-1.0-SNAPSHOT-fat.jar stop -cluster clique.verticles.UserInitHandler
-java -jar ./UserLikesHandler/target/UserLikesHandler-1.0-SNAPSHOT-fat.jar stop -cluster clique.verticles.UserLikesHandler
-java -jar ./UserTaggedPlacesHandler/target/UserTaggedPlacesHandler-1.0-SNAPSHOT-fat.jar stop -cluster clique.verticles.UserTokenHandler
-java -jar ./UserTokenHandler/target/UserTokenHandler-1.0-SNAPSHOT-fat.jar stop -cluster clique.verticles.UserTokenHandler
-java -jar ./Web/target/Web-1.0-SNAPSHOT-fat.jar stop -cluster clique.verticles.FacebookAuthenticate
+vertx=$(java -jar target/clique-1.0-SNAPSHOT-fat.jar list | awk '{ print $1 }' | grep -v Listing)
+
+for i in $vertx
+do 
+	java -jar target/clique-1.0-SNAPSHOT-fat.jar stop $i
+done
